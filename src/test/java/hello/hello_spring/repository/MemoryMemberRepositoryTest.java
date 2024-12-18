4b6e540b02b28f655a4751bb91bb84418a458edb
seolhee-choi <csheeee38@gmail.com>
2024-12-18 16:34:57 +0900

    섹션4 회원 리포지토리 케이스 작성중

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+//        Assertions.assertEquals(member, result);
+    }
+}
